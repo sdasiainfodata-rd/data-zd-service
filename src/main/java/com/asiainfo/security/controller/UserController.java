91f62ef67bcfed290226e84301a7a5ccd4d4cdac
Hadoop <1011161615@qq.com>
2019-10-09 17:28:49 +0800

    添加了菜单管理

--- src/main/java/com/asiainfo/security/controller/UserController.java
+++ src/main/java/com/asiainfo/security/controller/UserController.java
@@ -12,8 +12,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 /**
