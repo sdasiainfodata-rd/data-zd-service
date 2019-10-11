3050270008773c4841d9bd1f4f36abc2d1b68fcc
Hadoop <1011161615@qq.com>
2019-10-11 16:25:54 +0800

    修改当拒绝访问时,返回的状态码为401

--- src/main/java/com/asiainfo/security/controller/UrlController.java
+++ src/main/java/com/asiainfo/security/controller/UrlController.java
@@ -17,8 +17,8 @@ public class UrlController {
 
     @RequestMapping("/refuse")
     private ResponseEntity toRefuse(){
-        HashMap<String, HttpStatus> status = new HashMap<>();
-        status.put("status", HttpStatus.UNAUTHORIZED);
+        HashMap<String, Integer> status = new HashMap<>();
+        status.put("status", 401);
         return new ResponseEntity(status, HttpStatus.UNAUTHORIZED);
     }
 }
